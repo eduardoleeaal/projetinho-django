4ab9f86dcd9cc3b0ee6d205484097ab88f6694d6
Eduardo Leal <eduardo.leeal@hotmail.com>
2022-05-19 12:42:31 -0300

    Remove contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -5,14 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Eduardo Leal'
     })
-    # return HTTP response
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
-    # return HTTP response
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-    # return HTTP response
